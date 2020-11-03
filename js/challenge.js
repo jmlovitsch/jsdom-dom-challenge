81785be7bf4ea081f652c26a38aba656051731d9
john <jmlovitsch@gmail.com>
2020-11-03 13:27:19 -0600

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,71 +1,61 @@
-function _toConsumableArray(a){
-    if(Array.isArray(a)) {
-        for(var b=0,
-            c=Array(a.length);
-        b<a.length;
-        b++)c[b]=a[b];
-        return c
-    }
-    return Array.from(a)
-}
+const counter = document.getElementById('counter')
+const ul = document.querySelector('.likes')
+let intervalCounter = setInterval(count, 1000)
+const minus = document.getElementById('minus')
+let playing=!0
+const pause = document.getElementById('pause')
+const commentForm = document.querySelector('form')
+const heart = document.getElementById('heart')
 
-var playing=!0,
-timer=function() {
-    return setInterval(function() {
-        var a=document.getElementById("counter"),
-        b=parseInt(a.innerText);
-        a.innerText=b+1
-    },
-    1e3)
-},
 
-interval=timer(),
-// minus=document.getElementById("minus"),
-// plus=document.getElementById("plus"),
-// heart=document.getElementById("heart"),
-pause=document.getElementById("pause"),
-commentForm=document.getElementsByTagName("form")[0];
 
-document.getElementById("minus").addEventListener("click",function() {
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText);
-    a.innerText=b-1
-}),
+const numLikesObj = {}
 
-document.getElementById("plus").addEventListener("click",function() {
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText);
-    a.innerText=b+1
-}),
-
-document.getElementById("heart").addEventListener("click",function() {
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText),
-    c=document.querySelector(".likes"),
-    d=void 0;
-    if([].concat(_toConsumableArray(c.children)).map(function(a) {
-        return parseInt(a.dataset.num)
-    }
-        ).includes(b)) {
-            d=document.querySelector('[data-num="'+b+'"]');
-            var e=parseInt(d.children[0].innerText);
-            d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"
+//counts -beginning
+function count() {
+    newNum = parseInt(counter.innerText) + 1
+    return counter.innerText = newNum}
+//counts -end
+heart.addEventListener('click', event => {
+    let li = document.createElement('li')
+    if (numLikesObj[counter.innerText] !== undefined) {
+        numLikesObj[counter.innerText] += 1
+        } else {
+        numLikesObj[counter.innerText] = 1
         }
-            else(d=document.createElement("li")).setAttribute("data-num",b),
-            d.innerHTML=b+" has been liked <span>1</span> time",
-            c.appendChild(d)
-        }),
+    li.innerText = `${newNum} has been liked ${numLikesObj[counter.innerText]} times!`
+    ul.append(li)
+})
 
-            pause.addEventListener("click",function() {
-                playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),this.innerText="pause"),
-                [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){
-                    "pause"!==a.id&&(a.disabled=!playing)
-                })
-            }),
-                    commentForm.addEventListener("submit",function(a) {
-                        a.preventDefault();
-                        var b=this.children[0],c=b.value;b.value="";
-                        var d=document.querySelector(".comments"),
-                        e=document.createElement("p");
-                        e.innerText=c,d.appendChild(e)
-                    });
+minus.addEventListener('click', event => {
+    let num = counter.innerText
+    newNum = parseInt(num) - 1
+    return counter.innerText = newNum
+})
+
+//top
+const plus = document.getElementById('plus')
+    plus.addEventListener('click', event => count())
+//bottom
+
+//top
+const ul2 = document.createElement('ul')
+
+const div = document.querySelector('#list')
+
+div.append(ul2)
+
+commentForm.addEventListener('submit', event=> {
+    event.preventDefault()
+    let li = document.createElement('li')
+
+    li.innerText = event.target[0].value
+
+    ul2.append(li)
+})
+
+count()
+
+pause.addEventListener("click", event => {
+    playing ? (playing=!1,clearInterval(intervalCounter),pause.innerText="resume") : (playing=!0,intervalCounter=setInterval(count, 1000),pause.innerText="pause")
+})
